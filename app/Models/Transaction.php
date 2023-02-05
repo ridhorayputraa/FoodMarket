e31e0b3c2d15bb2adcfe733c32ff02d100d7d9a5
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-05 20:25:27 +0700

    adding config for Transaction Model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,43 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+         protected $fillable = [
+            "food_id", "user_id", 'quantity', 'total', 'status',
+            'payment_url'
+        ];
+
+    protected $guarded = [
+        'id'
+    ];
+
+
+// Relation
+public function food(){
+    return $this->hasOne(Food::class, 'id', 'food_id');
+}
+
+public function user(){
+    return $this->hasOne(User::class, 'id', 'user_id');
+}
+
+    // epoch untuk FE
+    public function getCreatedAtAttribute($value) {
+        // membuat assesor untuk mengakses file yang sudah ada
+        return Carbon::parse($value)->timestamp;
+    }
+
+    // epoch untuk FE
+    public function getUpdatedAtAttribute($value) {
+        // membuat assesor untuk mengakses file yang sudah ada
+        return Carbon::parse($value)->timestamp;
+    }
 }

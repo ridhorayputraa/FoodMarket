320e64234d5cfbc73dd2dd49eea58f30f8a6c5fd
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-05 22:00:40 +0700

    adding API/UserController

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -32,13 +32,11 @@ public function user(){
 
     // epoch untuk FE
     public function getCreatedAtAttribute($value) {
-        // membuat assesor untuk mengakses file yang sudah ada
         return Carbon::parse($value)->timestamp;
     }
 
     // epoch untuk FE
     public function getUpdatedAtAttribute($value) {
-        // membuat assesor untuk mengakses file yang sudah ada
         return Carbon::parse($value)->timestamp;
     }
 }

40eebd20cb15732aeff448bbccefb22b417e1b45
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-15 15:26:57 +0700

    jika hash tidak sesuai dengan auth

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,6 +20,24 @@ class UserController extends Controller
                 'email' => 'email|required',
                 'password' => 'required'
             ]);
+
+            // Mengecek credentials (login)
+            $credentials = request(['email', 'password']);
+
+            if(!Auth::attempt($credentials)){
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500 );
+            }
+
+            // Jika hash tidak sesuai maka beri error
+            $user = User::where('email', $request->email)->first();
+            if(!Hash::check($request->password, $user->password, [])){
+                throw new \Exception('Invalid Credentials');
+            }
+
+            
+
         }
     }
 }

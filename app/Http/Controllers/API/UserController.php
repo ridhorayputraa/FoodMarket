ed4e54b4c9872343cb79f97bd6a6025a3c82500e
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-15 15:40:53 +0700

    create function if fulfilled then login, indeed adding catch

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -36,8 +37,19 @@ class UserController extends Controller
                 throw new \Exception('Invalid Credentials');
             }
 
-            
+            // Jika berhasil maka loginkan
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return  ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ], 'Authenticated');
 
+        } catch(Exception $error){
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error
+            ], 'Authentication Failed', 500);
         }
     }
 }

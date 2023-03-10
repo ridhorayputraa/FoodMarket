f37d15a85a7b7ffbc14bf441449eb1e387ccda3c
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-10 18:52:54 +0700

    adding FoodController

---
+++ app/Http/Controllers/API/FoodController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FoodController extends Controller
+{
+    // 1 fungsi ini  akan menghandle semua request
+    // perlu menyiapkan beberapa opsi untuk filter
+    // Berdasarkan ID, Berdasarlan Harga, Berdasarkan Tipe
+}

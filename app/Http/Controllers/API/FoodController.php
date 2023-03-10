0b282dd5961a961afdefef171c8d2440b4c1e5e6
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-10 18:57:40 +0700

    adding filtering by Id

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -10,4 +10,12 @@ class FoodController extends Controller
     // 1 fungsi ini  akan menghandle semua request
     // perlu menyiapkan beberapa opsi untuk filter
     // Berdasarkan ID, Berdasarlan Harga, Berdasarkan Tipe
+
+    public function all(Request $request){
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        // LIMIT membatasi hanya 6 Request
+        $name = $request->input('name');
+        $types = $request->input('types');
+    }
 }

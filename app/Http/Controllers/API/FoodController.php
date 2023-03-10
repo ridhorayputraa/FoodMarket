d04a82f2ce145f94cfb4ea74f4d133f99ff4c00a
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-10 19:01:19 +0700

    adding filtering by price

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -17,5 +17,13 @@ class FoodController extends Controller
         // LIMIT membatasi hanya 6 Request
         $name = $request->input('name');
         $types = $request->input('types');
+
+
+        // Filtering berdasarkan harga
+        // terkecil -> terbesar
+        $price_from = $request->input('price_from');
+        $price_to = $request->input('price_to');
+
+        
     }
 }

1c6c97421bcaaf1ece79c2fa342eb4cc8db1173d
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-15 13:14:27 +0700

    adding transaction controller

--- app/Http/Controllers/API/FoodController.php
+++ app/Http/Controllers/API/FoodController.php
@@ -9,9 +9,7 @@ use Illuminate\Http\Request;
 
 class FoodController extends Controller
 {
-    // 1 fungsi ini  akan menghandle semua request
-    // perlu menyiapkan beberapa opsi untuk filter
-    // Berdasarkan ID, Berdasarlan Harga, Berdasarkan Tipe
+   
 
     public function all(Request $request){
         $id = $request->input('id');

e71c962c1383e6ec5d8cb3587a56488fd97d5eab
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-19 07:30:41 +0700

    create MidtransController and create Callback Method

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MidtransController extends Controller
+{
+    //
+    public function callback(Request $request){
+        // Set Konfigurasi Midtrans
+
+        // Buat instance Midtrans Notification
+
+        // Assign ke variable untuk memudahkan codingan
+
+        // Cari transaksi berdasarkan ID
+
+        // Handle Notifikasi status Midtrans
+
+        // Simpan Transaksi
+    }
+}

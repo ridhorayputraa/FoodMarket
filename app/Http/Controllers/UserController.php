c70f4f5943019748069bd93f0d9636cdc0e4d9cb
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-03-21 17:23:41 +0700

    di method index kita pnggil user dan return datanya beserta view di folder users index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +14,12 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        // buat variable user dang panggil data user pagination 10
+        $user = User::paginate(10);
+
+        return view('users.index', [
+            'user' => $user
+        ]);
     }
 
     /**
